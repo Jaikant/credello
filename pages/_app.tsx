ce87c068f4b38044125756f980dac565ca8e6279
Jai <jaikant@gmail.com>
2020-07-21 12:46:24 +0530

    cross browser fonts?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,14 +8,27 @@ import { useEffect } from 'react';
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
-    font-family: Roboto;
-      src: url('/static/fonts/Roboto-Regular.ttf'),
-      src: url('/static/fonts/Roboto-Medium.ttf'),
-      src: url('/static/fonts/Roboto-Bold.ttf'),
-      src: url('/static/fonts/Roboto-Black.ttf'),
+    font-family: "Roboto-Regular";
+      src: url('/static/fonts/Roboto-Regular.ttf') format("truetype"),
   }
-  * {
-    font-family: Roboto;
+  @font-face {
+    font-family: "Roboto-Medium";
+      src: url('/static/fonts/Roboto-Medium.ttf') format("truetype"),
+  }
+  @font-face {
+    font-family: "Roboto-Bold";
+      src: url('/static/fonts/Roboto-Bold.ttf') format("truetype"),
+  }
+  @font-face {
+    font-family: "Roboto-Black";
+      src: url('/static/fonts/Roboto-Black.ttf') format("truetype"),
+  }
+
+  body {
+    font-family: Roboto-Regular;
+  }
+  h1, h2, h3, h4, h5, h6 {
+    font-family: Roboto-Bold;
   }
 `;
 
